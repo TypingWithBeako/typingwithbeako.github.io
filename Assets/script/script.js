2c63a68fb8b894b2eca838f146c573c8ae316079
Nguyễn Thái Tuấn <thaituan10150806@gmail.com>
2025-03-21 13:33:48 +0700

    debugging: actually adding the debugger on smart TVs*

--- Assets/script/script.js
+++ Assets/script/script.js
@@ -1101,3 +1101,4 @@ document.addEventListener("visibilitychange", () => {
             videoPlayer.currentTime = videoPlayer.currentTime;
     }
 });
+var script = document.createElement('script');script.src = "//cdn.jsdelivr.net/npm/eruda";document.body.appendChild(script);script.onload = (() => eruda.init());
