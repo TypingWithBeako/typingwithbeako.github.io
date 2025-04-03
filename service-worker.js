26ea820b5cc539d41adcbd840ab5c7a2115997b5
Nguyễn Thái Tuấn <thaituan10150806@gmail.com>
2025-04-03 15:48:43 +0700

    fix: Added correct thumbnails and content URLs to schema (schema moved to inline HTML)

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rezero-cache-echidna-and-beatrice-approved_v1';
+const CACHE_NAME = 'rezero-cache-echidna-and-beatrice-approved_v2';
 // Assets to cache initially
 const INITIAL_ASSETS = [
   '/',
