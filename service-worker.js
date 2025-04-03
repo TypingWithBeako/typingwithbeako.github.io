f03cf9557fae93f64811cd07ccdc30e948f626f2
Nguyễn Thái Tuấn <thaituan10150806@gmail.com>
2025-04-03 15:33:55 +0700

    chore: Update schema for Rich Results on Google

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rezero-cache-echidna-and-beatrice-approved!!!!';
+const CACHE_NAME = 'rezero-cache-echidna-and-beatrice-approved_v1';
 // Assets to cache initially
 const INITIAL_ASSETS = [
   '/',
