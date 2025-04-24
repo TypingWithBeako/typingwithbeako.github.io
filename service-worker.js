88d7794e883da2a3879a540a9159456ee08d3840
Nguyễn Thái Tuấn <thaituan10150806@gmail.com>
2025-04-24 13:27:16 +0700

    chore: Added visual indicator (color change) on looping button

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rezero-cache-echidna-and-beatrice-approved-finished-v8';
+const CACHE_NAME = 'rezero-cache-echidna-and-beatrice-approved-finished-v9';
 // Assets to cache initially
 const INITIAL_ASSETS = [
   '/',
