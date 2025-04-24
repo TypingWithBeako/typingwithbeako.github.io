dd3a31f9bb318bd8536ec22a1d770a7fc89506ee
Nguyễn Thái Tuấn <thaituan10150806@gmail.com>
2025-04-24 16:26:01 +0700

    chore: Finishing touches for the color (#7771b3 to #9a8ad4)

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rezero-cache-echidna-and-beatrice-approved-finished-v9';
+const CACHE_NAME = 'rezero-cache-echidna-and-beatrice-approved-finished-I_suppose';
 // Assets to cache initially
 const INITIAL_ASSETS = [
   '/',
