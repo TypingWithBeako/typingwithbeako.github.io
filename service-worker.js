fb5cdd85a386805b53f903fd90efa2a3693d1ba0
Nguyễn Thái Tuấn <thaituan10150806@gmail.com>
2025-05-06 20:00:48 +0700

    fix(cache): Force cache refresh to address incorrect cache configuration

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rezero-cache-echidna-and-beatrice-approved-finished-I_suppose_v1';
+const CACHE_NAME = 'rezero-cache-echidna-and-beatrice-approved-finished-I_suppose_v2';
 // Assets to cache initially
 const INITIAL_ASSETS = [
   '/',
