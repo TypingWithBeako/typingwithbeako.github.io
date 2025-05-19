04ef3fc708310c0aae844f3743ad1a2baf9e7616
Nguyễn Thái Tuấn <thaituan10150806@gmail.com>
2025-05-20 02:15:08 +0700

    chore: Stylizing "Myth & Roid" to full caps.

--- Assets/script/script.js
+++ Assets/script/script.js
@@ -103,7 +103,7 @@ videoPlayer.addEventListener('play',function(){
     let artist = ''
     let musicArtwork = ''
     if (name == 'STYX HELIX' || name == 'Paradisus - Paradoxum' || name == 'NOX LUX' ||name == 'STRAIGHT BET' || name == 'S1 Ending' || name == 'Theater D' || name == 'STYX HELIX slow' || name == 'STYX HELIX nocut'){ 
-        artist = 'Myth & Roid'
+        artist = 'MYTH & ROID'
         if (name == 'STYX HELIX')
             musicArtwork = {
                 src: "Icons/artworks/STYX_HELIX_Cover.webp",
@@ -151,7 +151,7 @@ videoPlayer.addEventListener('play',function(){
               }
         }
         else if (name == 'STYX HELIX nocut') {
-            artist = 'Myth & Roid'
+            name = "STYX HELIX"
             musicArtwork = {
                 src: "Icons/artworks/STYX_HELIX_Cover.webp",
                 sizes: "1008x1000",
