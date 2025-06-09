19487874467decd17c7841fb200219d98b10c519
Nguyễn Thái Tuấn <thaituan10150806@gmail.com>
2025-06-09 09:34:19 +0700

    fix: Responsve tweaks (again)

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rezero-cache-echidna-and-beatrice-approved-finished-toast-v24';
+const CACHE_NAME = 'rezero-cache-echidna-and-beatrice-approved-finished-toast-v25';
 // Assets to cache initially
 const INITIAL_ASSETS = [
   '/',
