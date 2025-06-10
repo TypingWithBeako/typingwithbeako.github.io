d6dffce49f2c1680fc01284664c944b7093dde7d
Nguyễn Thái Tuấn <thaituan10150806@gmail.com>
2025-06-10 18:39:11 +0700

    fix: Responsive changes on mobile*

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rezero-cache-echidna-and-beatrice-approved-finished-toast-v26';
+const CACHE_NAME = 'rezero-cache-echidna-and-beatrice-approved-finished-toast-v27';
 // Assets to cache initially
 const INITIAL_ASSETS = [
   '/',
