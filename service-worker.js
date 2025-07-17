2c85fe3e99b4f74a11ec373a094d13b385278631
Nguyễn Thái Tuấn <thaituan10150806@gmail.com>
2025-07-17 17:51:50 +0700

    fix(input): Handle non-numeric key codes in handleNumberKeyPress correctly

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rezero-cache-echidna-and-beatrice-approved-finished-toast-speech-v36';
+const CACHE_NAME = 'rezero-cache-echidna-and-beatrice-approved-finished-toast-speech-v37';
 // Assets to cache initially
 const INITIAL_ASSETS = [
   '/',
